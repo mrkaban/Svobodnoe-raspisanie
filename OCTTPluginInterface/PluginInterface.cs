c3ca730d0fb0700edc2d523a9d418731b73d60e8
Алексей <s-r-grass@mail.ru>
2021-03-30 21:29:21 +0700

    Add files via upload

---
+++ OCTTPluginInterface/PluginInterface.cs
@@ -0,0 +1,71 @@
+#region Open Course Timetabler - An application for school and university course timetabling
+//
+// Author:
+//   Ivan Æurak (mailto:Ivan.Curak@fesb.hr)
+//
+// Copyright (c) 2007 Ivan Æurak, Split, Croatia
+//
+// http://www.openctt.org
+//
+//This file is part of Open Course Timetabler.
+//
+//Open Course Timetabler is free software;
+//you can redistribute it and/or modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2 of the License,
+//or (at your option) any later version.
+//
+//Open Course Timetabler is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+//or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along with
+//Open Course Timetabler; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+//Fifth Floor, Boston, MA  02110-1301  USA
+
+#endregion
+
+
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace OCTTPluginInterface
+{
+	public interface IPlugin
+	{
+		IPluginHost Host {get;set;}
+		
+		string Name {get;}
+		string Description {get;}
+		string Author {get;}
+		string Version {get;}
+
+		string MITextForHost {get;}
+		MenuItem HostMenuItemForActivation{get;set;}
+
+		//1 -menu item is enabled only if document is closed
+		//2 -menu item is enabled only if document is open
+		//3 -menu item is allways enabled
+		int EnabledDisabledMIType{get;}
+		
+		
+		System.Windows.Forms.Form MainGUIForm {get;}
+		
+		void Initialize();
+		void Dispose();
+	
+	}
+	
+	public interface IPluginHost
+	{		
+		string ActiveDocumentData_Test{get;}
+
+		DataSet OpenCTTDataSet{get;}
+		bool closeActiveDocument();
+
+		void importDataForTTStart(DataSet ds);
+                	
+
+	}
+
+
+}
